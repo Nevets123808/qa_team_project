0a46314d4b8731ae0a8a91ea71eec24ab2f7e3e3
Tommy Calvin <tommycalvin@Tommys-MacBook-Pro.local>
2021-08-09 12:36:35 +0100

    Required variable VPC (Host VPC) to be declared in root to create HTTP rule

---
+++ terraform/sg/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+   region = var.region
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow http inbound traffic"
+#   vpc_id      = aws_vpc.main.id
+
+  ingress = [
+    {
+      description      = "http from VPC"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    }
+  ]
+
+  egress = [
+    {
+      from_port        = 80
+      to_port          = 80
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
+
+  tags = {
+    Name = "allow_http"
+  }
+}

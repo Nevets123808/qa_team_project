44f9481372d6929b5d334519eacd13041985b8f7
Steven Bourn <stevenbourn2@hotmail.com>
2021-08-09 12:30:52 +0100

    basic node group config

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_cluster" "DeploymentCluster" {
-    name = "deploymentcluster"
+    name = "deploymentCluster"
     # role_arn = var.eks_role_id
     vpc_config {
         subnet_ids = [var.public_subnet_id, var.private_subnet_id]
@@ -7,5 +7,15 @@ resource "aws_eks_cluster" "DeploymentCluster" {
 }
 
 resource "aws_eks_node_group" "DeploymentNodes"{
-    
+    cluster_name = "deploymentCluster"
+    node_group_name = "deploymentNodeGroup"
+    node_role_arn = var.node_role_arn
+    subnet_ids = [var.public_subnet_id, var.private_subnet_id]
+
+    scaling_config {
+        #We might want to make these variables so they can be set at top-level
+        desired_size = 1
+        max_size = 1
+        min_size = 1
+    }
 }

ed492f89fb89bb0290ba2db3ad44d9b0a3300949
Ubuntu <ubuntu@ip-172-31-12-224.eu-west-2.compute.internal>
2021-08-09 14:20:31 +0000

    ic

---
+++ terraform/igw/main.tf
@@ -0,0 +1,30 @@
+resource "aws_internet_gateway" "igw" {
+
+    vpc_id = var.vpc_id
+
+    tags = {
+        Name = "IGW"
+    }
+
+}
+
+resource "aws_route_table" "rt" {
+    vpc_id = var.vpc_id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.vpc_id
+    }
+    tags = {
+        Name = "Route Table for VPC"
+    }
+}
+
+resource "aws_route_table_association" "public_subnet_rta" {
+    subnet_id = var.subnet_1
+    route_table_id = aws_route_table.rt.vpc_id
+}
+
+resource "aws_route_table_association" "public_subnet_rta" {
+    subnet_id = var.subnet_2
+    route_table_id = aws_route_table.rt.vpc_id
+}

74c15dc69e08f6a0c50fcfd30e062b46daa2b567
Steven Bourn <stevenbourn2@hotmail.com>
2021-08-09 17:55:19 +0100

    changes for terraform deployment

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -6,9 +6,9 @@ variable "node_role_arn"{
     default = "arn:aws:iam::830645951449:role/EKSClusterRole"
 }
 variable "public_subnet_id"{
-    default = "subnet-14bfc56e"
+    
 }
 
 variable "private_subnet_id"{
-    default = "subnet-76fb733a"
+    
 }

1e00f7cb1550a2752a9de6ee8386a4021443f7f5
Tommy Calvin <tommycalvin@Tommys-MacBook-Pro.local>
2021-08-12 11:51:51 +0100

    Updated Terraform

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -5,4 +5,7 @@ provider "aws" {
 resource "aws_instance" "node" {
     ami = var.ami
     instance_type = var.instance_size
+    associate_public_ip_address = var.enable_public_ip
+    subnet_id                   = var.subnet_id
+    vpc_security_group_ids      = var.vpc_security_group_ids
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,9 @@ module "ec2" {
     ami = var.ami
     instance_size = var.instance_size
     region = var.region
+    subnet_id = modules.subnet.public_subnet_id
+    vpc_security_group_ids = modules.sg.sg_id
+    associate_public_ip_address = true
 }
 
 module "eks" {
